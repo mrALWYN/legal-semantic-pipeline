e33ea10d407891467151d65d3d20fdb152111020
mrALWYN <alwynantonyben@gmail.com>
2025-11-12 10:57:52 +0530

    cicd pipline pseudo

---
+++ tests/test_pipeline.py
@@ -0,0 +1,8 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_health():
+    response = client.get("/")
+    assert response.status_code in [200, 404]
